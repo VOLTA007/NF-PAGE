0b5c9509de59abf55f1f1f53732094592be60996
VOLTA0007 <volta@gmail.com>
2024-04-21 11:03:49 +0200

    Commit

--- pages/api/UserLogin/index.js
+++ pages/api/UserLogin/index.js
@@ -1,24 +1,10 @@
 import { createRouter } from 'next-connect'
-import cors from 'cors'
 import dbConnectLogin from '@/utils/dbConnectLogin'
 import UserLogin from '@/models/UserLogin'
 
 dbConnectLogin()
 
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 204,
-}
-
-const router = createRouter()
-
-// Apply CORS middleware to the router
-router.use(cors(corsOptions))
-
-// Define your POST route handler
-router.post(async (req, res) => {
+const router = createRouter().post(async (req, res) => {
     try {
         const { email, password } = req.body
 
@@ -26,18 +12,18 @@ router.post(async (req, res) => {
         const user = await UserLogin.findOne({ email })
 
         if (!user) {
-            return res.status(401).json({
-                success: false,
-                message: 'Invalid email or password',
-            })
+            // If user does not exist, return an error
+            return res
+                .status(401)
+                .json({ success: false, message: 'Invalid email or password' })
         }
 
         // Compare the provided password with the password stored in the database
         if (user.password !== password) {
-            return res.status(401).json({
-                success: false,
-                message: 'Invalid email or password',
-            })
+            // If passwords do not match, return an error
+            return res
+                .status(401)
+                .json({ success: false, message: 'Invalid email or password' })
         }
 
         // Authentication successful
