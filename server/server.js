9e98efc05efa7daba0984235b1f8647d7f5076b7
VOLTA0007 <volta@gmail.com>
2024-04-21 10:54:09 +0200

    Commit

--- server/server.js
+++ server/server.js
@@ -1,75 +1,53 @@
-const http = require('http');
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors'); // Import CORS module
-// const socketIo = require('socket.io'); 
+const http = require('http')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors') // Import CORS module
+// const socketIo = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
 // Enable CORS for all routes (adjust if needed)
 const io = require('socket.io')(server, {
-  cors:{
-   origin: '*',
-   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], 
-   credentials: true,
-   optionsSuccessStatus: 204,
-  }
-});
+    cors: {
+        origin: '*',
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        credentials: true,
+        optionsSuccessStatus: 204,
+    },
+})
 
-
-app.use(cors());  
-
-
+app.use(cors())
 
 // Connect to MongoDB (replace with your connection string)
-mongoose.connect('mongodb+srv://volta0007:X5913lock1@cluster0.l8jlgt1.mongodb.net/UserLogin')
-.then(() => {
-  console.log("Connected to MongoDB");
-}).catch(err => {
-  console.error("MongoDB connection error:", err);
-});
+mongoose
+    .connect(
+        'mongodb+srv://volta0007:X5913lock1@cluster0.l8jlgt1.mongodb.net/UserLogin'
+    )
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err)
+    })
 
 // Get the UserLogin model (replace with your model path)
-const UserLogin = require('../models/UserLogin');
+const UserLogin = require('../models/UserLogin')
 
 // Listen for changes in the MongoDB UserLogin collection
-const changeStream = UserLogin.watch();
+const changeStream = UserLogin.watch()
 
-changeStream.on('change', change => {
-  console.log("Emitting dataUpdate event with change:", change);
-  // Push updated data to clients
-  io.emit('dataUpdate', change);
-});
+changeStream.on('change', (change) => {
+    console.log('Emitting dataUpdate event with change:', change)
+    // Push updated data to clients
+    io.emit('dataUpdate', change)
+})
 
 // Start the server
-const PORT = process.env.PORT || 3001; // Use environment variable or default to 3001
+const PORT = process.env.PORT || 3001 // Use environment variable or default to 3001
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(`Server is running on port ${PORT}`)
+})
 
 // const http = require('http');
 
@@ -89,8 +67,6 @@ server.listen(PORT, () => {
 //   socket.handshake.headers.origin = 'http://localhost:3000';
 //   socket.emit('Access-Control-Allow-Origin', 'http://localhost:3000');
 // });
-
-
 
 // // Connect to MongoDB
 // mongoose.connect('mongodb+srv://volta0007:X5913lock1@cluster0.l8jlgt1.mongodb.net/UserLogin',).then(() => {
