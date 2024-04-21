d7b5f90a063105c71de9749beea0c51e390108c8
VOLTA0007 <volta@gmail.com>
2024-04-21 16:22:33 +0200

    Commit

--- Components/PayPage.js
+++ Components/PayPage.js
@@ -3,8 +3,11 @@ import { generateKashierOrderHash } from '@/utils/generateKashierOrderHash'
 import config from '@/utils/Config2'
 import domain from '@/utils/Config'
 import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
 
 const PayPage = ({ amount, currency }) => {
+
+    const { data: session } = useSession()
     const router = useRouter()
 
     const { baseUrl } = config
@@ -20,7 +23,7 @@ const PayPage = ({ amount, currency }) => {
         secret: configObj.PaymentApiKey,
         baseUrl: configObj.baseUrl,
         metaData: JSON.stringify({
-            'Cutomer Email': 'mkhalid@kashier.io',
+            'Cutomer Email': `${session.user.email}`,
         }),
         merchantRedirect: `${domain}/payment-redirect`,
         display: 'en',
--- pages/api/payment-redirect.js
+++ pages/api/payment-redirect.js
@@ -1,60 +1,51 @@
 import dbConnectLogin from '@/utils/dbConnectLogin'
 import UserLogin from '@/models/UserLogin'
-import { useSession } from 'next-auth/react'
 
 dbConnectLogin()
 
- const { data: session } = useSession()
-
 export default async function handler(req, res) {
-
     try {
-        // Retrieve the user session
-       
         // Parse query parameters from the request URL
-        const { paymentStatus } = req.query
+        const { paymentStatus, email } = req.query // Extract email from query parameters
 
-        if (!session) {
-            console.log('Session not found')
-            return res.status(401).json({ error: 'User session not found' })
+        if (!email) {
+            console.log('Customer email not provided')
+            return res
+                .status(400)
+                .json({ error: 'Customer email not provided' })
         }
 
         if (paymentStatus === 'SUCCESS') {
             // Payment successful
-            if (session) {
-                // Find user by email and update is_subscribed to true
-                const userEmail = session.user.email
-                const updatedUser = await UserLogin.findOneAndUpdate(
-                    { email: userEmail },
-                    { is_subscribed: true },
-                    { new: true }
-                )
+            const updatedUser = await UserLogin.findOneAndUpdate(
+                { email },
+                { is_subscribed: true },
+                { new: true }
+            )
 
-                if (updatedUser) {
-                    console.log('User subscription updated successfully')
-                    res.writeHead(302, { Location: '/Home' })
-                    res.end()
-                } else {
-                    console.log('User not found or update failed')
-                    res.status(404).json({
-                        error: 'User not found or update failed',
-                    })
-                }
+            if (updatedUser) {
+                console.log('User subscription updated successfully')
+                res.writeHead(302, { Location: '/Home' })
+                return res.end()
             } else {
-                console.log('Session not found')
-                res.status(401).json({ error: 'User session not found' })
+                console.log('User not found or update failed')
+                return res.status(404).json({
+                    error: 'User not found or update failed',
+                })
             }
         } else if (paymentStatus === 'FAILED') {
             // Payment failed
             console.log('Payment failed')
-            res.status(200).json({ message: 'Payment failed' })
+            return res.status(200).json({ message: 'Payment failed' })
         } else {
             // Invalid payment status
             console.log('Invalid payment status')
-            res.status(400).json({ error: 'Invalid payment status' })
+            return res.status(400).json({ error: 'Invalid payment status' })
         }
     } catch (error) {
         console.error('Error processing payment response:', error)
-        res.status(500).json({ error: 'Error processing payment response' })
+        return res
+            .status(500)
+            .json({ error: 'Error processing payment response' })
     }
 }
