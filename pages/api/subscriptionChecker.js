8330d57222a7e1033c28de33530da28f6206ae32
VOLTA0007 <volta@gmail.com>
2024-04-23 23:12:16 +0200

    Commit

--- pages/api/subscriptionChecker.js
+++ pages/api/subscriptionChecker.js
@@ -1,19 +1,21 @@
-const cron = require('node-cron')
-const UserLogin = require('../../models/UserLogin')
-const dbcon2 = require('../../utils/dbcon2')
 
-dbcon2()
 
-// Define a cron job to run every second
-cron.schedule('* * * * * *', async () => {
+import dbConnectLogin from '@/utils/dbConnectLogin'
+import UserLogin from '@/models/UserLogin'
+
+// Connect to the database
+dbConnectLogin()
+
+// Define the API handler function
+export default async function handler(req, res) {
     try {
         const currentDate = new Date()
         const result = await UserLogin.updateMany(
             {
                 subscription_expiration_date: {
-                    $lte: currentDate, // Find documents where subscription_expiration_date is less than or equal to today
+                    $lte: currentDate,
                 },
-                is_subscribed: true, // Only update documents that are currently subscribed
+                is_subscribed: true,
             },
             {
                 $set: {
@@ -26,7 +28,12 @@ cron.schedule('* * * * * *', async () => {
         console.log(
             `Subscription status updated for ${result.nModified} user(s).`
         )
+
+        res.status(200).json({
+            message: `Subscription status updated for ${result.nModified} user(s).`,
+        })
     } catch (error) {
         console.error('Error updating subscription status:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
     }
-})
+}
