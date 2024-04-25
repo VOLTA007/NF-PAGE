3a13de30d44fe91e63692dc49394db9a8309c4ca
VOLTA0007 <volta@gmail.com>
2024-04-25 15:53:26 +0200

    Commit

--- pages/Academy/Pricing/[planId]-[selectedCountry].js
+++ pages/Academy/Pricing/[planId]-[selectedCountry].js
@@ -42,18 +42,32 @@ const PricingPlanCountry = () => {
 
     return (
         <>
-            <h1 className="text-5xl">Payment Details</h1>
-            <h1>Subscription Plan: {planId}</h1>
-            <h2>Selected Currency: {selectedCountry}</h2>
-            {amounta && (
-                <p>
-                    Amount:{' '}
-                    {selectedCountry === 'EGP'
-                        ? `${amounta} EGP`
-                        : `$${amounta} USD`}
-                </p>
-            )}
-            <PayPage amount={amounta} currency={selectedCountry} />
+            <div style={{ paddingTop: '20px' }}></div>
+            <div className="lg:flex justify-center items-center pl-4 pr-4">
+                <div className="bg-[linear-gradient(45deg,transparent_25%,rgba(68,68,68,.2)_50%,transparent_75%,transparent_100%)] bg-grey-950 overflow-hidden rounded-xl border border-grey-900 bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat text-nowrap lg:mx-[300px] mx-3 px-[10px] py-3 shadow-2xl transition-[background-position_0s_ease] hover:bg-[position:200%_0,0_0] hover:duration-[1500ms]">
+                    <div className="grid py-1 justify-start place-items-center mx-auto max-w-8xl"></div>
+                    <h1 className="text-4xl lg:text-5xl pb-4 text-center lg:pb-4 lg:pt-4">
+                        Checkout Details
+                    </h1>
+                    <h1 className="pb-4">Subscription Plan: {planId}</h1>
+                    <h2 className="pb-4">
+                        Selected Currency: {selectedCountry}
+                    </h2>
+                    {amounta && (
+                        <p className="pb-4">
+                            Amount:{' '}
+                            {selectedCountry === 'EGP'
+                                ? `${amounta} EGP`
+                                : `$${amounta} USD`}
+                        </p>
+                    )}
+                    <PayPage amount={amounta} currency={selectedCountry} />
+                </div>
+            </div>
+            <div
+                className="h-screen"
+                style={{ height: 'calc(100vh - 560px)' }}
+            ></div>
         </>
     )
 }
