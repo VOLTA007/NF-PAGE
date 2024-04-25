d9665437bbd465ba67e30f83ed6bc32d6c57efc8
VOLTA0007 <volta@gmail.com>
2024-04-25 15:26:18 +0200

    Commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import PageWrapper from '@/Components/PageWrapper'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }) {
     const router = useRouter()
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }) {
         <PageWrapper>
             <Component {...pageProps} />
             <Analytics />
+            <SpeedInsights />
         </PageWrapper>
     )
 }
