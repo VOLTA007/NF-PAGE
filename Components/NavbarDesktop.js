0a9c2f75c2ab2e73ef5eafc4228f28520e3083dd
VOLTA0007 <volta@gmail.com>
2024-04-29 22:57:05 +0300

    Commit

--- Components/NavbarDesktop.js
+++ Components/NavbarDesktop.js
@@ -74,13 +74,18 @@ const NavbarDesktop = () => {
                             </Link>
                         </>
                     )}
-                    <Link
-                        className='relative before:rounded-[12px] rounded-[15px] text-black transition-[padding] duration-[0.3s] ease-[ease-in-out] pl-10 pr-8 pt-4 pb-2 border-[3px] border-solid border-[#776e62] before:content-[""] before:absolute before:z-[-1] before:h-full before:w-full before:bg-white before:transition-all before:duration-[0.3s] before:ease-[ease-in-out] before:border-r-[3px] before:border-r-white before:border-b-[3px] before:border-b-white before:border-solid before:left-2 before:top-2 hover:px-9 hover:py-3 hover:before:left-0 hover:before:top-0 
-                        -webkit-transition: padding 0.3s  before:-webkit-transition: all 0.3s'
-                        href="/Academy/Profile"
-                    >
-                        My Profile
-                    </Link>
+
+                    {status === 'authenticated' && (
+                        <>
+                            <Link
+                                className={styles.btnhover}
+                                href="/Academy/Profile"
+                            >
+                                My Profile
+                            </Link>
+                        </>
+                    )}
+
                     {status === 'authenticated' && (
                         <>
                             <div className="w-[68.96px]">
