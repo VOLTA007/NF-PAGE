d6e3a2608233310a05fd8eb6affe683bff725b09
VOLTA0007 <volta@gmail.com>
2024-04-30 12:41:14 +0300

    Commit

--- Components/PayPage.js
+++ Components/PayPage.js
@@ -3,6 +3,7 @@ import { generateKashierOrderHash } from '@/utils/generateKashierOrderHash'
 import config from '@/utils/Config2'
 import domain from '@/utils/Config'
 import { useRouter } from 'next/router'
+import { Button } from '@nextui-org/react'
 
 const PayPage = ({ amount, currency }) => {
     const router = useRouter()
@@ -20,7 +21,7 @@ const PayPage = ({ amount, currency }) => {
         secret: configObj.PaymentApiKey,
         baseUrl: configObj.baseUrl,
         metaData: JSON.stringify({
-            'Cutomer Email': ``,
+            'Cutomer Email': '',
         }),
         merchantRedirect: `${domain}/payment-redirect`,
         display: 'en',
@@ -30,66 +31,47 @@ const PayPage = ({ amount, currency }) => {
         brandColor: 'rgba(163, 0, 0, 1)',
     }
 
-    const handleDivClick = (event) => {
-        event.preventDefault()
+    const handleButtonClick = () => {
+        const hppUrl =
+            `${configObj.baseUrl}?` +
+            `merchantId=${order.mid}` +
+            `&orderId=${order.merchantOrderId}` +
+            `&amount=${order.amount}` +
+            `&currency=${order.currency}` +
+            `&hash=${generateKashierOrderHash(order)}` +
+            `&merchantRedirect=${order.merchantRedirect}` +
+            `&metaData=${
+                order.metaData ? encodeURIComponent(order.metaData) : ''
+            }` +
+            `&allowedMethods=${
+                order.allowedMethods ? order.allowedMethods : ''
+            }` +
+            `&failureRedirect=${
+                order.failureRedirect ? order.failureRedirect : ''
+            }` +
+            `&redirectMethod=${
+                order.redirectMethod ? order.redirectMethod : ''
+            }` +
+            `&display=${order.display ? order.display : ''}` +
+            `&brandColor=${encodeURIComponent(order.brandColor)}` +
+            `&mode=${configObj.mode}`
+
+        // Open hppUrl in a new tab
+        const newTab = window.open(hppUrl, '_blank')
 
         // Navigate to /Academy/Profile
         router.push('/Academy/Profile')
-
-        // Open hppUrl in a new tab
-        window.open(hppUrl, '_blank')
     }
 
-
-    order.hash = generateKashierOrderHash(order)
-
-    const hppUrl =
-        `${configObj.baseUrl}?` +
-        `merchantId=${order.mid}` +
-        `&orderId=${order.merchantOrderId}` +
-        `&amount=${order.amount}` +
-        `&currency=${order.currency}` +
-        `&hash=${order.hash}` +
-        `&merchantRedirect=${order.merchantRedirect}` +
-        `&metaData=${order.metaData ? order.metaData : ''}` +
-        `&allowedMethods=${order.allowedMethods ? order.allowedMethods : ''}` +
-        `&failureRedirect=${
-            order.failureRedirect ? order.failureRedirect : ''
-        }` +
-        `&redirectMethod=${order.redirectMethod ? order.redirectMethod : ''}` +
-        `&display=${order.display ? order.display : ''}` +
-        `&brandColor=${encodeURIComponent(order.brandColor)}` +
-        `&mode=${configObj.mode}`
-
     return (
-        <div onClick={handleDivClick}>
-            <a
-                className="bg-red-500 rounded-sm px-2 py-2"
-                href={hppUrl}
-                target="_blank"
-                rel="noopener noreferrer"
+        <div>
+            <Button
+                radius="full"
+                className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+                onClick={handleButtonClick}
             >
-                Pay Now !
-            </a>
-            <br />
-
-            <script
-                id="kashier-iFrame"
-                src={`${baseUrl}/kashier-checkout.js`}
-                data-amount={order.amount}
-                data-hash={order.hash}
-                data-currency={order.currency}
-                data-orderId={order.merchantOrderId}
-                data-merchantId={order.mid}
-                data-merchantRedirect={order.merchantRedirect}
-                data-display={order.display}
-                data-mode={config.mode}
-                data-metaData={order.metaData}
-                data-redirectMethod={order.redirectMethod}
-                data-failureRedirect={order.failureRedirect}
-                data-allowedMethods={order.allowedMethods}
-                data-brandcolor={order.brandColor}
-            ></script>
+                Pay Now!
+            </Button>
         </div>
     )
 }
