de38a1d483713dc45e96868fde99c03e833f81e9
VOLTA0007 <volta@gmail.com>
2024-05-01 01:30:45 +0300

    Commit

--- Components/PricingComp.js
+++ Components/PricingComp.js
@@ -13,6 +13,7 @@ import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { io } from 'socket.io-client'
 import axios from 'axios'
+import { Card, CardFooter, Image, Button } from '@nextui-org/react'
 
 
 export const PricingComp = () => {
@@ -94,13 +95,35 @@ export const PricingComp = () => {
             <div style={{ paddingTop: '40px' }}></div>
             {subtrue ? (
                 <>
-                    <div className="flex items-center justify-center">
-                        You are Subscribed !
+                    <div className="flex justify-center items-center">
+                        <Card
+                            isFooterBlurred
+                            radius="lg"
+                            className="border-none"
+                        >
+                            <Image
+                                alt="Woman listing to music"
+                                className="object-cover"
+                                height={200}
+                                src="/diamond.gif"
+                                width={200}
+                            />
+                            <CardFooter className=" justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                                <p className="text-tiny text-black ">
+                                    Your are Subscribed
+                                </p>
+                                <Button
+                                    className="text-tiny bg-black/20 text-black"
+                                    variant="flat"
+                                    color="default"
+                                    radius="lg"
+                                    size="sm"
+                                >
+                                    My Profile
+                                </Button>
+                            </CardFooter>
+                        </Card>
                     </div>
-                    <div
-                        className="h-screen"
-                        style={{ height: 'calc(100vh - 360px)' }}
-                    ></div>
                 </>
             ) : (
                 <>
