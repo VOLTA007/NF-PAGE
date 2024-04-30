f4e6c2c6c321e72cdd267b249447f0b9a784f72d
VOLTA0007 <volta@gmail.com>
2024-05-01 01:20:07 +0300

    Commit

--- Components/NavbarDesktop.js
+++ Components/NavbarDesktop.js
@@ -37,24 +37,21 @@ const NavbarDesktop = () => {
                     />
                     <div className="flex justify-center items-center gap-10">
                         <Link
-                            className='relative before:rounded-[12px] rounded-[15px] text-black transition-[padding] duration-[0.3s] ease-[ease-in-out] pl-10 pr-8 pt-4 pb-2 border-[3px] border-solid border-[#776e62] before:content-[""] before:absolute before:z-[-1] before:h-full before:w-full before:bg-white before:transition-all before:duration-[0.3s] before:ease-[ease-in-out] before:border-r-[3px] before:border-r-white before:border-b-[3px] before:border-b-white before:border-solid before:left-2 before:top-2 hover:px-9 hover:py-3 hover:before:left-0 hover:before:top-0 
-                        -webkit-transition: padding 0.3s  before:-webkit-transition: all 0.3s'
+                            className={styles.btnhover}
                             href="/Academy/Home"
                             scroll={false}
                         >
                             Home
                         </Link>
                         <Link
-                            className='relative before:rounded-[12px] rounded-[15px] text-black transition-[padding] duration-[0.3s] ease-[ease-in-out] pl-10 pr-8 pt-4 pb-2 border-[3px] border-solid border-[#776e62] before:content-[""] before:absolute before:z-[-1] before:h-full before:w-full before:bg-white before:transition-all before:duration-[0.3s] before:ease-[ease-in-out] before:border-r-[3px] before:border-r-white before:border-b-[3px] before:border-b-white before:border-solid before:left-2 before:top-2 hover:px-9 hover:py-3 hover:before:left-0 hover:before:top-0 
-                        -webkit-transition: padding 0.3s  before:-webkit-transition: all 0.3s'
+                            className={styles.btnhover}
                             href="/Academy/Pricing"
                             scroll={false}
                         >
                             Subscribe
                         </Link>
                         <Link
-                            className='relative before:rounded-[12px] rounded-[15px] text-black transition-[padding] duration-[0.3s] ease-[ease-in-out] pl-10 pr-8 pt-4 pb-2 border-[3px] border-solid border-[#776e62] before:content-[""] before:absolute before:z-[-1] before:h-full before:w-full before:bg-white before:transition-all before:duration-[0.3s] before:ease-[ease-in-out] before:border-r-[3px] before:border-r-white before:border-b-[3px] before:border-b-white before:border-solid before:left-2 before:top-2 hover:px-9 hover:py-3 hover:before:left-0 hover:before:top-0 
-                        -webkit-transition: padding 0.3s  before:-webkit-transition: all 0.3s'
+                            className={styles.btnhover}
                             href="/Academy/About"
                             scroll={false}
                         >
