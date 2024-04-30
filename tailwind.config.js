936ddaa1b1f44e34b20e81ca777c84e538732779
VOLTA0007 <volta@gmail.com>
2024-04-30 14:13:25 +0300

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ const { nextui } = require('@nextui-org/react')
 module.exports = {
     content: [
         './pages/**/*.{js,ts,jsx,tsx}', // Include pages
-        './components/**/*.{js,ts,jsx,tsx}', // Include components (ensure lowercase 'components' directory)
+        './Components/**/*.{js,ts,jsx,tsx}', // Include components (ensure lowercase 'components' directory)
         './app/**/*.{js,ts,jsx,tsx}', // Include app files
         './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}', // Include NextUI theme files
     ],
