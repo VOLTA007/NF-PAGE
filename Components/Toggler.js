fedf63543b3b49035d5ab040a4134f34dba57fd0
VOLTA0007 <volta@gmail.com>
2024-05-01 02:45:02 +0300

    Commit

--- Components/Toggler.js
+++ Components/Toggler.js
@@ -4,6 +4,8 @@ import { BsSunFill } from 'react-icons/bs'
 import { useRecoilState } from 'recoil'
 import { isDarkS } from '../utils/recoilstate3'
 
+import { Switch } from '@nextui-org/react'
+
 const Toggler = () => {
     const [darkMode, setDarkMode] = useRecoilState(isDarkS)
 
@@ -42,22 +44,15 @@ const Toggler = () => {
 
     return (
         <div className="flex justify-end mt-4 mr-4">
-            <div
-                className="relative w-16 h-8 flex items-center bg-gray-400 dark:bg-slate-600 cursor-pointer rounded-full p-1"
-                onClick={toggleDarkMode}
-            >
-                <FaMoon className="text-white" size={18} />
-
-                <div
-                    className={`absolute bg-white dark:bg-slate-550 w-6 h-6 rounded-full shadow-md transition-transform duration-300 ${
-                        darkMode ? 'translate-x-8' : 'translate-x-0'
-                    }`}
-                ></div>
-
-                <BsSunFill
-                    className="ml-auto bg-yellow-500 text-white rounded-full p-1"
-                    size={18}
-                />
+            <div className="relative w-16 h-8 flex items-center cursor-pointer rounded-full p-1">
+                <Switch
+                    defaultSelected
+                    size="lg"
+                    color="success"
+                    startContent={<FaMoon />}
+                    endContent={<BsSunFill />}
+                    onClick={toggleDarkMode}
+                ></Switch>
             </div>
         </div>
     )
