1736f910e51b041bed1fb51c018766272e2b26df
VOLTA0007 <volta@gmail.com>
2024-04-30 14:51:36 +0300

    Commit

--- pages/Academy/Home/index.js
+++ pages/Academy/Home/index.js
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Userwelcome from '@/Components/Userwelcome'
 import { Image } from '@nextui-org/react'
-
-
-
+import { useMediaQuery } from '@react-hook/media-query'
 
 export default function Home() {
+    const [isMobileWidth, setIsMobileWidth] = useState(null)
+
+    const isMobileWidthHook = useMediaQuery('(max-width: 1023px)') // Adjust width breakpoint as needed
+
+    useEffect(() => {
+        setIsMobileWidth(isMobileWidthHook)
+    }, [isMobileWidthHook])
+
+    // Set different image widths based on isMobileWidth
+    const imageWidth = isMobileWidth ? 320 : 550
+
     return (
         <>
             <div className="bg-[#fffaf6] dark:bg-slate-800 w-fill h-fit rounded-[30px] lg:mx-10 mt-4 mx-2">
                 <Userwelcome />
                 <div style={{ paddingBottom: '40px' }}></div>
 
-                <div className="grid md:grid-cols-[repeat(2,1fr)] grid-rows-[1fr] gap-x-0 gap-y-0 ">
-                    <div className="flex justify-center ">
+                <div className="grid md:grid-cols-[repeat(2,1fr)] grid-rows-[1fr] gap-x-0 gap-y-0">
+                    <div className="flex justify-center">
                         <h1 className="md:text-7xl text-3xl inter-unique slant-10 text-center">
-                            IM Abdalrahman,<br></br> your Nutritionist
+                            IM Abdalrahman,
+                            <br />
+                            your Nutritionist
                         </h1>
                     </div>
-                    <div className=" flex justify-center mr-10 md:mt-0 mt-6">
+                    <div className="flex justify-center mr- md:mt-0 mt-6">
                         <Image
                             isBlurred
-                            width={310}
+                            width={imageWidth} // Use the calculated imageWidth
                             src="/Abdo.jpg"
                             alt="Doctor Image"
                             className="m-5"
