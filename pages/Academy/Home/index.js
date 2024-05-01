f8fe0ee471b4dcab4502b2b3ac3047a191d2252e
VOLTA0007 <volta@gmail.com>
2024-05-01 03:02:33 +0300

    Commit

--- pages/Academy/Home/index.js
+++ pages/Academy/Home/index.js
@@ -91,7 +91,7 @@ export default function Home() {
                                 />
                                 <CardFooter className=" justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                                     <p className="text-tiny text-black dark:text-white ">
-                                        See from our people
+                                        Our people Opinions
                                     </p>
                                     <Button
                                         className="text-tiny bg-black/20 text-black  dark:text-white"
